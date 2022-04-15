645fba881ac66c63474db8bd7d3911eb2f58d8ac
Tetiana Stasiuk <tanastasuk35@gmail.com>
2022-04-15 16:02:05 +0300

    _Layout has been changed. Register and Login templates have been created

---
+++ InvestOA/InvestOA.WebApp/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvestOA.WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
